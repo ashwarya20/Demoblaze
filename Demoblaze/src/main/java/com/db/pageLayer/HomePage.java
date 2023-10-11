bd3227369de82f3de5a67439f6954d92130793e6
ashwarya20 <aishnagargoje@gmail.com>
2023-10-11 17:36:44 +0530

    Task Updated

---
+++ Demoblaze/src/main/java/com/db/pageLayer/HomePage.java
@@ -0,0 +1,13 @@
+package com.db.pageLayer;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+	   public HomePage(WebDriver driver) {
+		   
+		   
+	   }
+	   
+	   
+}
---
+++ Demoblaze/src/test/java/com/db/TestLayer/TestCase.java
@@ -0,0 +1,5 @@
+package com.db.TestLayer;
+
+public class TestCase {
+
+}
